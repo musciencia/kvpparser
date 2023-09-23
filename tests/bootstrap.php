c51ff909da092db568639fe4a521296ccf92fc83
Francisco <musciencia@gmail.com>
2023-09-23 12:13:23 -0400

    I'm trying to set up the package so that tests files are not included when the package is installed with composer
    That's why I added the "archive" section
    Also removed the "autoload-dev" from composer.json
    and set it up in the boostrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$classLoader = require __DIR__ . '/../vendor/autoload.php';
+
+// Add namespace to autoloader
+// Using here instead of adding to composer.json 
+// so that we can exclude test files wen package is intalled
+$classLoader->addPsr4('Tests\\Unit\\', __DIR__ . '/unit');
